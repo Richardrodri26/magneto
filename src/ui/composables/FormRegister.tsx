417bb93dd49bb52da8385af2f52fc7d5cefd0e42
Richard Rodriguez <richardmanuel811@gmail.com>
2024-03-07 21:51:23 -0500

    first commit

---
+++ src/ui/composables/FormRegister.tsx
@@ -0,0 +1,197 @@
+import { Checkbox, Input, RadioGroup, RadioGroupItem, Select, SelectContent, SelectItem, SelectTrigger, SelectValue, Switch, Textarea } from "@/components"
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage, } from "@/components/Form"
+import { Link } from "lucide-react";
+
+
+type InputFormBasicType = {
+  name: string;
+  placeholder?: string
+  label?: string;
+  description?: string | React.ReactNode
+}
+
+interface InputFormInterface extends InputFormBasicType { }
+
+export const InputForm = ({ name, label, placeholder, description }: InputFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input placeholder={placeholder} {...field} />
+          </FormControl>
+          <FormDescription>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+interface CheckboxFormInterface extends InputFormBasicType { }
+
+export const CheckBoxForm = ({ name, description, label }: CheckboxFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow">
+          <FormControl>
+            <Checkbox
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+          </FormControl>
+          <div className="space-y-1 leading-none">
+            <FormLabel>
+              {label}
+            </FormLabel>
+            <FormDescription>
+              {description}
+            </FormDescription>
+          </div>
+        </FormItem>
+      )}
+    />
+  )
+
+}
+
+interface RadioGroupFormInterface extends InputFormBasicType {
+  children: React.ReactNode
+
+}
+
+export const RadioGroupForm = ({ name, label, children }: RadioGroupFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem className="space-y-3">
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="flex flex-col space-y-1"
+            >
+              {children}
+            </RadioGroup>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+interface RadioGroupItemFormInterface {
+  value: string;
+  label?: string;
+}
+
+export const RadioGroupItemForm = ({ value, label }: RadioGroupItemFormInterface) => {
+
+  return (
+    <FormItem className="flex items-center space-x-3 space-y-0">
+      <FormControl>
+        <RadioGroupItem value={value} />
+      </FormControl>
+      <FormLabel className="font-normal">
+        {label}
+      </FormLabel>
+    </FormItem>
+  )
+}
+
+interface SelectFormInterface extends InputFormBasicType { }
+
+export const SelectForm = ({ name, label, description }: SelectFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a verified email to display" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem value="m@example.com">m@example.com</SelectItem>
+              <SelectItem value="m@google.com">m@google.com</SelectItem>
+              <SelectItem value="m@support.com">m@support.com</SelectItem>
+            </SelectContent>
+          </Select>
+          <FormDescription>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+interface SwitchFormInterface extends InputFormBasicType { }
+
+export const SwitchForm = ({ name, description, label }: SwitchFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <div className="space-y-0.5">
+            <FormLabel>{label}</FormLabel>
+            <FormDescription>
+              {description}
+            </FormDescription>
+          </div>
+          <FormControl>
+            <Switch
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  )
+}
+
+interface TextareaFormInterface extends InputFormBasicType { }
+
+export const TextareaForm = ({ name, description, label, placeholder }: TextareaFormInterface) => {
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Textarea
+              placeholder={placeholder}
+              className="resize-none"
+              {...field}
+            />
+          </FormControl>
+          <FormDescription>
+            {description}
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
